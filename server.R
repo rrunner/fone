d03860c68a77236f8df0b6ef1230968a2a515ff5
rrunner <therainbowrunner@gmail.com>
2014-08-14 00:08:43 +0200

    enable caching: functions

--- server.R
+++ server.R
@@ -13,65 +13,99 @@ map_theme <- theme(axis.line = element_blank(),
 
 map <- ggplot() + coord_cartesian(ylim=c(-60, 70), xlim=c(-150, 180)) + borders(database="world", colour="grey", fill="darkseagreen") + map_theme
 
-# download file with race locations for any year
+
+
+
+# list of location data (to enable caching)
+loc_lst <- new.env(parent=emptyenv())
+loc_lst <- vector(mode="list", length=length(seq(year(today()), 1950, by=-1)))
+loc_lst <- setNames(loc_lst, paste0("y", seq(year(today()), 1950, by=-1)))
+
+# download file with race locations for a year
 download_locations <- function(year) {
-  url <- paste0("http://ergast.com/api/f1/",year,".json")
-  fromJSON(url, encoding="utf-8")
+  if (!is.null(loc_lst[[paste0("y", year)]])) return(invisible(0))
+  url <- paste0("http://ergast.com/api/f1/", year, ".json")
+  loc_lst[[paste0("y", year)]] <<- fromJSON(url, encoding="utf-8")
+  return(invisible(0))
 }
 
 # transform lists of location data into a data.frame object
-convert_locations <- function(d, no_races = length(d)) {
-  local_data <- data.frame()
-  current_date <- Sys.Date()
+convert_locations <- function(d, no_races=length(d)) {
+  local_data <- data.frame(round=integer(no_races),
+                           circuit=character(no_races),
+                           lat=numeric(no_races),
+                           long=numeric(no_races),
+                           city=character(no_races),
+                           event_occurred=logical(no_races),
+                           stringsAsFactors=FALSE)
 
-  for (i in (1:no_races)) {
-    temp_data <- data.frame(d[[i]]$round,
-                            d[[i]]$Circuit$circuitName,
-                            as.numeric(d[[i]]$Circuit$Location[1]),
-                            as.numeric(d[[i]]$Circuit$Location[2]),
-                            unname(d[[i]]$Circuit$Location[3]),
-                            as.Date(d[[i]]$date) < current_date)
-    local_data <- rbind(local_data, temp_data)
+  for (i in seq(no_races)) {
+    local_data[i, ] <- c(d[[i]]$round,
+                         d[[i]]$Circuit$circuitName,
+                         as.numeric(d[[i]]$Circuit$Location[1]),
+                         as.numeric(d[[i]]$Circuit$Location[2]),
+                         unname(d[[i]]$Circuit$Location[3]),
+                         as.Date(d[[i]]$date) < Sys.Date())
   }
 
-  names(local_data) <- c("round", "circuit", "lat", "long", "city",
-                         "event_occurred")
   local_data
 }
 
-# download file with race results for any given race and year
+
+
+
+# list of result data (to enable caching), defaults to max 30 races per year
+res_lst <- new.env(parent=emptyenv())
+res_lst <- vector(mode="list", length=length(seq(year(today()), 1950, by=-1)))
+res_lst <- setNames(res_lst, paste0("y", seq(year(today()), 1950, by=-1)))
+res_lst <- lapply(res_lst, function(x) vector(mode="list", length=30))
+
+# download file with race results for a given race and year
 download_results <- function(year, round) {
+  if (!is.null((res_lst[[paste0("y", year)]][[round]]))) return(invisible(0))
   url <- paste("http://ergast.com/api/f1", year, round, "results.json", sep="/")
-  fromJSON(url, encoding="utf-8")
+  res_lst[[paste0("y", year)]][[round]] <<- fromJSON(url, encoding="utf-8")
+  return(invisible(0))
 }
 
 # transform lists of results data into a data.frame object
-get_result_list <- function(d, no_drivers = length(d)) {
-  result_list <- data.frame()
+get_result_list <- function(d, no_drivers=length(d)) {
+  result_list <- data.frame(Position=integer(no_drivers),
+                            Number=integer(no_drivers),
+                            Driver=character(no_drivers),
+                            Constructor=character(no_drivers),
+                            Laps=integer(no_drivers),
+                            Grid=integer(no_drivers),
+                            Status=character(no_drivers),
+                            Points=numeric(no_drivers),
+                            stringsAsFactors=FALSE)
 
-  for (i in (1:no_drivers)) {
-    temp_data <- data.frame(d[[i]]$position,
-                            d[[i]]$number,
-                            paste(d[[i]]$Driver["givenName"],
-                                  d[[i]]$Driver["familyName"], sep=" "),
-                            d[[i]]$Constructor["name"],
-                            d[[i]]$laps,
-                            d[[i]]$grid,
-                            d[[i]]$status,
-                            d[[i]]$points)
-    result_list <- rbind(result_list, temp_data)
+  for (i in seq(no_drivers)) {
+    result_list[i, ] <- c(d[[i]]$position,
+                          d[[i]]$number,
+                          paste(d[[i]]$Driver["givenName"],
+                                d[[i]]$Driver["familyName"], sep=" "),
+                          d[[i]]$Constructor["name"],
+                          d[[i]]$laps,
+                          d[[i]]$grid,
+                          d[[i]]$status,
+                          d[[i]]$points)
   }
 
-  names(result_list) <- c("Position", "Number", "Driver", "Constructor", "Laps",
-                          "Grid", "Status", "Points")
-  row.names(result_list) <- NULL
   result_list
 }
+
+
+
+
 
 # get position for a given circuit
 get_position <- function(d, circuit) {
   d[which(d$circuit == circuit), c("lat", "long")]
 }
+
+
+
 
 # server logic
 shinyServer(function(input, output, session) {
