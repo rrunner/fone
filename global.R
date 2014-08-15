19be0319ff401a600d91c6f3f6dea652889eeb30
rrunner <therainbowrunner@gmail.com>
2014-08-16 01:21:57 +0200

    remove dependency on lubridate package

--- global.R
+++ global.R
@@ -1,15 +1,15 @@
-library(lubridate)
 library(RJSONIO)
 
 # Create global variable "last_year" to use in server.R and ui.R,
 # should the Ergast database not be loaded with location data before
 # the start of a new year.
-check_location_data_available <- function(yyyy=year(today())) {
+check_location_data_available <- function(yyyy) {
   url <- paste0("http://ergast.com/api/f1/", yyyy, ".json")
   temp <- fromJSON(url, encoding="utf-8")
   if (length(temp$MRData$RaceTable$Races) > 0) return(yyyy)
   check_location_data_available(yyyy - 1)
 }
 
-last_year <- check_location_data_available()
-rm(check_location_data_available)
+current_year <- as.numeric(format(Sys.time(), "%Y"))
+last_year <- check_location_data_available(current_year)
+rm(check_location_data_available, current_year)
