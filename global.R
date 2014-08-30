d2605691079d3429308bd573c6484cabefc14224
rrunner <therainbowrunner@gmail.com>
2014-08-30 16:58:11 +0200

    rename last_race to last_round

--- global.R
+++ global.R
@@ -1,8 +1,8 @@
 library(RJSONIO)
 
-# Create global variable "last_year" and "last_race" to use in server.R and ui.R
+# Create global variable "last_year" and "last_round" to use in server.R and ui.R
 #   - fetch the latest available race results in Ergast database
 temp <- fromJSON("http://ergast.com/api/f1/current/last.json", encoding="utf-8")
 last_year <- as.numeric(temp$MRData$RaceTable$season)
-last_race <- as.numeric(temp$MRData$RaceTable$round)
+last_round <- as.numeric(temp$MRData$RaceTable$round)
 rm(temp)
