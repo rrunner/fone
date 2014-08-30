fcd8af33080a18f785aff6b4368976744d57252e
rrunner <therainbowrunner@gmail.com>
2014-08-30 16:17:57 +0200

    simplified retrieval of last_year and last_race

--- global.R
+++ global.R
@@ -1,15 +1,8 @@
 library(RJSONIO)
 
-# Create global variable "last_year" to use in server.R and ui.R,
-# should the Ergast database not be loaded with location data before
-# the start of a new year.
-check_location_data_available <- function(yyyy) {
-  url <- paste0("http://ergast.com/api/f1/", yyyy, ".json")
-  temp <- fromJSON(url, encoding="utf-8")
-  if (length(temp$MRData$RaceTable$Races) > 0) return(yyyy)
-  check_location_data_available(yyyy - 1)
-}
-
-current_year <- as.numeric(format(Sys.time(), "%Y"))
-last_year <- check_location_data_available(current_year)
-rm(check_location_data_available, current_year)
+# Create global variable "last_year" and "last_race" to use in server.R and ui.R
+#   - fetch the latest available race results in Ergast database
+temp <- fromJSON("http://ergast.com/api/f1/current/last.json", encoding="utf-8")
+last_year <- as.numeric(temp$MRData$RaceTable$season)
+last_race <- as.numeric(temp$MRData$RaceTable$round)
+rm(temp)
