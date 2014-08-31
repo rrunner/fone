9de0e01b9cd9658ae63032e62dfb606986da8511
rrunner <therainbowrunner@gmail.com>
2014-08-31 16:23:28 +0200

    cosmetics

--- server.R
+++ server.R
@@ -6,46 +6,52 @@ library(RJSONIO)
 current_year <- as.numeric(format(Sys.Date(), "%Y"))
 
 # store location data (mutable environment to enable caching)
-loc <- new.env(parent=emptyenv())
-loc <- vector(mode="list", length=length(seq(current_year, 1950, by=-1)))
-loc <- setNames(loc, paste0("y", seq(current_year, 1950, by=-1)))
+loc <- new.env(parent = emptyenv())
+loc <- vector(mode = "list", length = length(seq(current_year, 1950, by = -1)))
+loc <- setNames(loc, paste0("y", seq(current_year, 1950, by = -1)))
 
 # store result data (mutable environment to enable caching)
-res <- new.env(parent=emptyenv())
-res <- vector(mode="list", length=length(seq(current_year, 1950, by=-1)))
-res <- setNames(res, paste0("y", seq(current_year, 1950, by=-1)))
+res <- new.env(parent = emptyenv())
+res <- vector(mode = "list", length = length(seq(current_year, 1950, by = -1)))
+res <- setNames(res, paste0("y", seq(current_year, 1950, by = -1)))
 
 # download race locations for a given year
 download_locations <- function(year) {
   if (!is.null(loc[[paste0("y", year)]])) return(invisible(0))
   url <- paste0("http://ergast.com/api/f1/", year, ".json")
-  temp <- fromJSON(url, encoding="utf-8")
+  temp <- fromJSON(url, encoding = "utf-8")
 
   # initialise result list for given year
   no_races <- as.numeric(temp$MRData$total)
-  res[[paste0("y", year)]] <<- vector(mode="list", length=no_races)
+  res[[paste0("y", year)]] <<- vector(mode = "list", length = no_races)
 
   loc[[paste0("y", year)]] <<- convert_locations(temp$MRData$RaceTable$Races)
   invisible(0)
 }
 
 # transform location data into a data.frame object
-convert_locations <- function(d, no_races=length(d)) {
-  local_data <- data.frame(round=integer(no_races),
-                           circuit=character(no_races),
-                           lat=numeric(no_races),
-                           long=numeric(no_races),
-                           city=character(no_races),
-                           url=character(no_races),
-                           stringsAsFactors=FALSE)
+convert_locations <- function(d, no_races = length(d)) {
+  local_data <-
+    data.frame(
+      round   = integer(no_races),
+      circuit = character(no_races),
+      lat     = numeric(no_races),
+      long    = numeric(no_races),
+      city    = character(no_races),
+      url     = character(no_races),
+      stringsAsFactors = FALSE
+      )
 
   for (i in seq(no_races)) {
-    local_data[i, ] <- list(as.integer(d[[i]]$round),
-                            d[[i]]$Circuit$circuitName,
-                            as.numeric(d[[i]]$Circuit$Location[1]),
-                            as.numeric(d[[i]]$Circuit$Location[2]),
-                            unname(d[[i]]$Circuit$Location[3]),
-                            d[[i]]$url)
+    local_data[i, ] <-
+      list(
+        as.integer(d[[i]]$round),
+        d[[i]]$Circuit$circuitName,
+        as.numeric(d[[i]]$Circuit$Location[1]),
+        as.numeric(d[[i]]$Circuit$Location[2]),
+        unname(d[[i]]$Circuit$Location[3]),
+        d[[i]]$url
+        )
   }
 
   local_data
@@ -54,32 +60,37 @@ convert_locations <- function(d, no_races=length(d)) {
 # download race results for a given year and race
 download_results <- function(year, round) {
   if (!is.null((res[[paste0("y", year)]][[round]]))) return(invisible(0))
-  url <- paste("http://ergast.com/api/f1", year, round, "results.json", sep="/")
-  temp <- fromJSON(url, encoding="utf-8")
+  url <- paste("http://ergast.com/api/f1", year, round, "results.json",
+               sep = "/")
+  temp <- fromJSON(url, encoding = "utf-8")
   res[[paste0("y", year)]][[round]] <<-
     convert_results(temp$MRData$RaceTable$Races[[1]]$Results)
   invisible(0)
 }
 
 # transform results data into a data.frame object
-convert_results <- function(d, no_drivers=length(d)) {
-  result_list <- data.frame(Position=integer(no_drivers),
-                            Number=integer(no_drivers),
-                            Driver=character(no_drivers),
-                            Constructor=character(no_drivers),
-                            Laps=integer(no_drivers),
-                            Grid=integer(no_drivers),
-                            Time=character(no_drivers),
-                            Status=character(no_drivers),
-                            Points=numeric(no_drivers),
-                            stringsAsFactors=FALSE)
+convert_results <- function(d, no_drivers = length(d)) {
+  result_list <-
+    data.frame(
+      Position    = integer(no_drivers),
+      Number      = integer(no_drivers),
+      Driver      = character(no_drivers),
+      Constructor = character(no_drivers),
+      Laps        = integer(no_drivers),
+      Grid        = integer(no_drivers),
+      Time        = character(no_drivers),
+      Status      = character(no_drivers),
+      Points      = numeric(no_drivers),
+      stringsAsFactors = FALSE
+      )
 
   for (i in seq(no_drivers)) {
     result_list[i, ] <-
       list(
         as.integer(d[[i]]$position),
         as.integer(d[[i]]$number),
-        paste(d[[i]]$Driver["givenName"], d[[i]]$Driver["familyName"], sep=" "),
+        paste(d[[i]]$Driver["givenName"], d[[i]]$Driver["familyName"],
+              sep = " "),
         d[[i]]$Constructor["name"],
         as.integer(d[[i]]$laps),
         as.integer(d[[i]]$grid),
@@ -115,7 +126,7 @@ shinyServer(function(input, output, session) {
   # focus "race location tab" when user selects another year
   observe({
     input$year
-    updateTabsetPanel(session, "whichTab", selected="location")
+    updateTabsetPanel(session, inputId = "whichTab", selected = "location")
   })
 
   # retrieve year and round of last updated race in Ergast database
@@ -123,7 +134,7 @@ shinyServer(function(input, output, session) {
     # make a poll every 10 minutes
     invalidateLater(6e+05, session)
     temp <- fromJSON("http://ergast.com/api/f1/current/last.json",
-                     encoding="utf-8")
+                     encoding = "utf-8")
     list(
       last_year  = as.numeric(temp$MRData$RaceTable$season),
       last_round = as.numeric(temp$MRData$RaceTable$round)
@@ -132,8 +143,8 @@ shinyServer(function(input, output, session) {
 
   # generate years dynamically (render UI)
   output$year_list <- renderUI({
-    selectInput(inputId='year', label='Select year:',
-                choices=c(seq(yr()$last_year, 1950, by=-1)))
+    selectInput(inputId = "year", label = "Select year:",
+                choices = c(seq(yr()$last_year, 1950, by = -1)))
   })
 
   # generate circuit list for past race events
@@ -149,8 +160,8 @@ shinyServer(function(input, output, session) {
 
   # generate circuits dynamically (render UI)
   output$circuit_list <- renderUI({
-    selectInput(inputId='circuit', label='Select circuit:',
-                choices=c("", circuits()))
+    selectInput(inputId = "circuit", label = "Select circuit:",
+                choices = c("", circuits()))
   })
 
   # retrieve result data
@@ -185,8 +196,12 @@ shinyServer(function(input, output, session) {
   # pass table to output
   output$table <- renderDataTable({
     result_data()
-  }, options=list(iDisplayLength=10,
-                  aLengthMenu=c(3, 10, nrow(result_data()))))
+    },
+    options = list(
+      iDisplayLength = 10,
+      aLengthMenu    = c(3, 10, nrow(result_data()))
+      )
+    )
 
   # pass Wikipedia link to output (render UI)
   output$wikipedia <- renderUI({
@@ -197,7 +212,7 @@ shinyServer(function(input, output, session) {
     # guardian if wikipedia link is not available in Ergast
     if (length(url) == 0) return()
 
-    tags$a(href=url, target="_blank", "Race info on Wikipedia")
+    tags$a(href = url, target = "_blank", "Race info on Wikipedia")
     })
 
   # pass text or table to output (render UI)
@@ -212,17 +227,17 @@ shinyServer(function(input, output, session) {
 
     # leaflet map
     lmap <- Leaflet$new()
-    lmap$set(width=850, height=420)
+    lmap$set(width = 850, height = 420)
 
     if (!is.null(input$circuit) && input$circuit != "") {
       pos <- get_position(positions, input$circuit)
-      lmap$setView(c(pos$lat, pos$long), zoom=12)
-      lmap$marker(c(pos$lat, pos$long), bindPopup=input$circuit)
+      lmap$setView(c(pos$lat, pos$long), zoom = 12)
+      lmap$marker(c(pos$lat, pos$long), bindPopup = input$circuit)
     } else {
-      lmap$setView(c(20, 15), zoom=2)
+      lmap$setView(c(20, 15), zoom = 2)
       for (i in seq(nrow(positions))) {
         lmap$marker(c(positions$lat[i], positions$long[i]),
-                    bindPopup=positions$circuit[i])
+                    bindPopup = positions$circuit[i])
       }
     }
 
--- ui.R
+++ ui.R
@@ -5,30 +5,31 @@ library(rCharts)
 shinyUI(fluidPage(
 
   # application title
-  titlePanel(title="Formula 1: Race locations and results",
-             windowTitle="fone"),
+  titlePanel(title       = "Formula 1: Race locations and results",
+             windowTitle = "fone"),
 
   # user inputs (rendered by server)
   fluidRow(
     column(2,
-           uiOutput('year_list')
+           uiOutput("year_list")
     ),
     column(8,
-           uiOutput('circuit_list')
+           uiOutput("circuit_list")
     )
   ),
 
   # display plot and result list in tabs
   mainPanel(
     tabsetPanel(
-      tabPanel("Race locations", showOutput('map', 'leaflet'), value="location"),
+      tabPanel("Race locations", showOutput("map", "leaflet"),
+               value = "location"),
       tabPanel("Race results",
                uiOutput("wikipedia"),
                tags$br(),
-               uiOutput("text_or_table"), value="result"),
-      id="whichTab",
-      type="pills"
+               uiOutput("text_or_table"), value = "result"),
+      id   = "whichTab",
+      type = "pills"
       ),
-    width=10)
+    width = 10)
   )
 )
